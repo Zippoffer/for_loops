0fc7cf88a835d4f28975f43de575b3b15459f715
Christopher Hill <thorsdaddy77@gmail.com>
2016-05-28 18:31:53 -0500

    for-loops

---
+++ for-loops.js
@@ -0,0 +1,74 @@
+// // for (var i = 5; i < 120; i+=10){
+// // console.log("i",i);
+// // }
+
+
+// // for (var i = 4096; i > 1; i /= 2) {
+// // 	console.log("i", i);
+// // }
+
+
+// var pres = ["George Washington","John Adams","Thomas Jefferson","James Madison","James Monroe","John Quincy Adams","Andrew Jackson",
+// "Martin Van Buren","William Henry Harrison","John Tyler","James K. Polk","Zachary Taylor","Millard Fillmore","Franklin Pierce","James Buchanan",
+// "Abraham Lincoln","Andrew Johnson","Ulysses S. Grant","Rutherford B. Hayes","James A. Gar","Chester Arthur","Grover Cleveland","Benjamin Harrison",
+// "Grover Cleveland","William McKinley","Theodore Roosevelt","William Howard Taft","Woodrow Wilson","Warren G. Harding","Calvin Coolidge","Herbert Hoover",
+// "Franklin D. Roosevelt","Harry S. Truman","Dwight D. Eisenhower","John F. Kennedy","Lyndon B. Johnson","Richard Nixon","Gerald Ford","Jimmy Carter","Ronald Reagan",
+// "George Bush","Bill Clinton","George W. Bush","Barack Obama"];
+
+// var leaders = pres.length;
+
+
+
+
+
+
+// for (var i = 0; i < leaders; i++) {
+// 	console.log(i+1 + pres[i]);
+// }
+
+// var obj = {a:1, b:2, c:3};
+    
+// for (var prop in obj) {
+//   console.log("obj." + prop + " = " + obj[prop]);
+// }
+
+// Output:
+// "obj.a = 1"
+// "obj.b = 2"
+// "obj.c = 3"
+
+
+// for(var i = 0, size = list.length; i < size ; i++){
+//    var item = list[i];
+//    if(matchesLabel(item)){
+//       someFunction(item);
+//    } 
+
+
+var  bugs = {
+  argentine: {},
+  army: {},
+  bigHeaded: {},
+  black: {},
+  bull: {},
+  carpenthreer: {},
+  crazy: {},
+  fire: {},
+  glider: {},
+  honeyPot: {},
+  jackJumper: {}
+}
+
+var prop = ["red", "black", "purple", "blue", "orange", "gray", "flower", "doorbell", "groove", "tiny", "beagle"];
+
+for (var i = 0;  i < prop.length; i++) 
+	
+
+
+for (bugs in bugs) {
+	console.log(bugs + " = " + prop[i++]);
+}
+
+
+
+
